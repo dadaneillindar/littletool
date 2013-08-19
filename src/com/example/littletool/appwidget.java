6585e550a391d4ff14fadc0e2860f917d5253290
DADA <dadaneillindar@gmail.com>
2013-08-19 15:50:40 +0800

    only date

--- src/com/example/littletool/appwidget.java
+++ src/com/example/littletool/appwidget.java
@@ -17,7 +17,7 @@ public class appwidget extends AppWidgetProvider {
 		Calendar c = Calendar.getInstance();
 		String hour_str = String.format("%1$X", c.get(Calendar.HOUR_OF_DAY));
 		String min_str = String.format("%1$X", c.get(Calendar.MINUTE));
-		String date_str = String.format("%1$X 年 %2$X 月 %3$X 日 星期 %4$X",c.get(Calendar.YEAR),c.get(Calendar.MONTH)+1,c.get(Calendar.DATE),c.get(Calendar.DAY_OF_WEEK)-1);
+		String date_str = String.format("%1$X 年 %2$X 月 %3$X 日",c.get(Calendar.YEAR),c.get(Calendar.MONTH)+1,c.get(Calendar.DATE));
 		
 		rv.setTextViewText(R.id.hour_label, hour_str);
 		rv.setTextViewText(R.id.minute_label, min_str); 
